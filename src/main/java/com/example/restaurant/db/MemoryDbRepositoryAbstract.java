7459f3ff10d6345c40b57adb53851391cdfb92b4
yerim <sontt4266@naver.com>
2022-06-03 16:18:45 +0900

    naver api_searchLocal test auth error

--- src/main/java/com/example/restaurant/db/MemoryDbRepositoryAbstract.java
+++ src/main/java/com/example/restaurant/db/MemoryDbRepositoryAbstract.java
@@ -22,17 +22,16 @@ abstract public class MemoryDbRepositoryAbstract<T extends MemoryDbEntity> imple
             // db 에 데이터가 없는 경우 (새로 생성)
             index++;
             entity.setIndex(index);
-            db.add(entity);
-            return entity;
+
         }else{
             // db 에 이미 데이터가 있는 경우
             var preIndex = optionalEntity.get().getIndex();
             entity.setIndex(preIndex);
 
             deleteById(preIndex);
-            db.add(entity);
-            return entity;
         }
+        db.add(entity);
+        return entity;
     }
 
     @Override
